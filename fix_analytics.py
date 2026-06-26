7703a08c2ccd3eda9bafea232f77efdee6cfbb37
agoodmantatu <agoodmantatu@gmail.com>
2026-06-26 23:59:11 +0530

    Add: Google Analytics G-FPVK6NQ94R

---
+++ fix_analytics.py
@@ -0,0 +1,17 @@
+﻿src = open("index.html", encoding="utf-8").read()
+
+GTAG = """  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-FPVK6NQ94R"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-FPVK6NQ94R');
+  </script>"""
+
+if "G-FPVK6NQ94R" in src:
+    print("Analytics already added")
+else:
+    src = src.replace("<head>", "<head>\n" + GTAG)
+    open("index.html", "w", encoding="utf-8").write(src)
+    print("Google Analytics added")
